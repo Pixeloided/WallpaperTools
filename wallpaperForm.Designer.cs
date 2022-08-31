fe1ee6750b7e1e41b98656d19ea04d3d4339e460
Pixeloided <pixeloidsemail@gmail.com>
2022-08-30 20:38:42 -0700

    first commit

---
+++ wallpaperForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Wallpaper_Tools
+{
+    partial class WallpaperForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openImage = new System.Windows.Forms.Button();
+            this.setWallpaper = new System.Windows.Forms.Button();
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.wallpaperStyle = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // openImage
+            // 
+            this.openImage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.openImage.Location = new System.Drawing.Point(12, 12);
+            this.openImage.Name = "openImage";
+            this.openImage.Size = new System.Drawing.Size(113, 22);
+            this.openImage.TabIndex = 0;
+            this.openImage.Text = "Open Image File";
+            this.openImage.UseVisualStyleBackColor = true;
+            this.openImage.Click += new System.EventHandler(this.OpenImageFile);
+            // 
+            // setWallpaper
+            // 
+            this.setWallpaper.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.setWallpaper.Location = new System.Drawing.Point(285, 12);
+            this.setWallpaper.Name = "setWallpaper";
+            this.setWallpaper.Size = new System.Drawing.Size(90, 22);
+            this.setWallpaper.TabIndex = 1;
+            this.setWallpaper.Text = "Set Wallpaper";
+            this.setWallpaper.UseVisualStyleBackColor = true;
+            this.setWallpaper.Click += new System.EventHandler(this.SetWallpaper);
+            //
+            // wallpaperStyle
+            //
+            this.wallpaperStyle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.wallpaperStyle.Location = new System.Drawing.Point(380, 13);
+            this.wallpaperStyle.Size = new System.Drawing.Size(90, 23);
+            this.wallpaperStyle.Items.Add("Fill");
+            this.wallpaperStyle.Items.Add("Fit");
+            this.wallpaperStyle.Items.Add("Span");
+            this.wallpaperStyle.Items.Add("Stretch");
+            this.wallpaperStyle.Items.Add("Tile");
+            this.wallpaperStyle.Items.Add("Center");
+            this.wallpaperStyle.Name = "wallpaperStyle";
+            this.wallpaperStyle.TabIndex = 1;
+            this.wallpaperStyle.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.wallpaperStyle.SelectedItem = "Fill";
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.Location = new System.Drawing.Point(131, 12);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(148, 22);
+            this.fileLabel.TabIndex = 2;
+            this.fileLabel.Text = "Choose a valid image file.";
+            this.fileLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // WallpaperForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 47);
+            this.Controls.Add(this.fileLabel);
+            this.Controls.Add(this.setWallpaper);
+            this.Controls.Add(this.wallpaperStyle);
+            this.Controls.Add(this.openImage);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(503, 86);
+            this.MinimumSize = new System.Drawing.Size(503, 86);
+            this.Name = "WallpaperForm";
+            this.Text = "Wallpaper Tools";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button setWallpaper;
+        private System.Windows.Forms.Label fileLabel;
+
+        private System.Windows.Forms.Button openImage;
+        private System.Windows.Forms.ComboBox wallpaperStyle;
+
+        #endregion
+    }
+}
---
+++ wallpaperForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Wallpaper_Tools
+{
+    public sealed class Wallpaper
+    {
+        Wallpaper() { }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style
+        {
+            Fill,
+            Fit,
+            Span,
+            Stretch,
+            Tile,
+            Center
+        }
+
+        public static void Set(Uri uri, Style style)
+        {
+            var s = new WebClient().OpenRead(uri.ToString());
+            if (s == null) throw new Exception("An error occured.");
+            
+            var img = Image.FromStream(s);
+            var tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, ImageFormat.Bmp);
+
+            var key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (key == null) throw new Exception("An error occured.");
+            switch (style)
+            {
+                case Style.Fill:
+                    key.SetValue(@"WallpaperStyle", 10.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                case Style.Fit:
+                    key.SetValue(@"WallpaperStyle", 6.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                case Style.Span:
+                    key.SetValue(@"WallpaperStyle", 22.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                case Style.Stretch:
+                    key.SetValue(@"WallpaperStyle", 2.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                case Style.Tile:
+                    key.SetValue(@"WallpaperStyle", 0.ToString());
+                    key.SetValue(@"TileWallpaper", 1.ToString());
+                    break;
+                case Style.Center:
+                    key.SetValue(@"WallpaperStyle", 0.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(style), style, "That's not an option.");
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tempPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+    public partial class WallpaperForm : Form
+    {
+        private string _imagePath;
+        private string _imageName;
+
+        public WallpaperForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SetWallpaper(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            var uri = new System.Uri(_imagePath);
+            switch (this.wallpaperStyle.SelectedItem)
+            {
+                case "Fill":
+                    Wallpaper.Set(uri, Wallpaper.Style.Fill);
+                    break;
+                case "Fit":
+                    Wallpaper.Set(uri, Wallpaper.Style.Fit);
+                    break;
+                case "Span":
+                    Wallpaper.Set(uri, Wallpaper.Style.Span);
+                    break;
+                case "Stretch":
+                    Wallpaper.Set(uri, Wallpaper.Style.Stretch);
+                    break;
+                case "Tile":
+                    Wallpaper.Set(uri, Wallpaper.Style.Tile);
+                    break;
+                case "Center":
+                    Wallpaper.Set(uri, Wallpaper.Style.Center);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(this.wallpaperStyle.SelectedItem),
+                        this.wallpaperStyle.SelectedItem, "That's not an option.");
+            }
+            MessageBox.Show("Successfully set wallpaper!", "Success!");
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void OpenImageFile(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "";
+            dialog.Filter = "Image Files|*.jpg;*.jpeg;";
+            dialog.InitialDirectory = @"C:\";
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                _imagePath = dialog.FileName;
+                int idx = _imagePath.LastIndexOf(@"\", StringComparison.Ordinal);
+                if (idx != -1) _imageName = _imagePath[(idx + 1)..];
+                fileLabel.Text = _imageName;
+            }
+        }
+    }
+}
